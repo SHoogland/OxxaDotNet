7c7cab75a783136a43e3b46c9d57c11301a86a36
Stephan Hoogland <stephan@shoogland.com>
2014-12-02 15:29:06 +0100

    OxxaClient started

---
+++ OxxaDotNet/OxxaApiConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace OxxaDotNet {
+    public static class OxxaApiConfig {
+        static OxxaApiConfig() {
+            UserName = ConfigurationManager.AppSettings["oxxa_auth_username"] ?? "";
+            Password = ConfigurationManager.AppSettings["oxxa_auth_password"] ?? "";
+        }
+
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+    }
+}
---
+++ OxxaDotNet/OxxaClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxxaDotNet {
+    public class OxxaClient {
+        private readonly string _userName;
+        private readonly string _password;
+        public OxxaClient(string userName, string password) {
+            _userName = userName ?? OxxaApiConfig.UserName;
+            _password = password ?? OxxaApiConfig.Password;
+        }
+    }
+}
