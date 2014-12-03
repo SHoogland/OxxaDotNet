237cffe0f105b8cc7f65e3ba4179d36c296da644
Stephan Hoogland <stephan@shoogland.com>
2014-12-03 12:08:04 +0100

    reorder Assert functions to see what goes wrong

--- OxxaDotNet.Test/DomainCheckTest.cs
+++ OxxaDotNet.Test/DomainCheckTest.cs
@@ -17,10 +17,10 @@ namespace OxxaDotNet.Test {
             });
 
             Assert.Equal("domain_check", response.Command);
+            Assert.Equal("Domeinnaam is bezet.", response.StatusDescription);
             Assert.Equal("XMLOK 10", response.StatusCode);
             Assert.Equal(testDomainTld, response.Tld);
             Assert.Equal(testDomainSld, response.Sld);
-            Assert.Equal("Domeinnaam is bezet.", response.StatusDescription);
             Assert.Equal(0.0, response.Price);
             Assert.Equal(true, response.Done);
             Assert.Equal(true, response.OrderComplete);
